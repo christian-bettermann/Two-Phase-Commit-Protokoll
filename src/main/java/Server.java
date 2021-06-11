16040bc77a75ccfa20c1bdc744ea31005a1e4c31
Christian Bettermann <christian.bettermann@atos.net>
2021-06-11 13:54:12 +0200

    initial messages from server to broker

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -1,7 +1,114 @@
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.util.concurrent.TimeUnit;
 
 public class Server {
 	
-	public Server(int Port) {
+	private static DatagramSocket socket;
+    private static boolean running;
+    private byte[] buffer = new byte[256];
+    
+    private static InetAddress carBrokerAddress;
+    private static InetAddress hotelBrokerAddress;
+	
+    private static DatagramSocket carBrokerSocket;
+    private static DatagramSocket hotelBrokerSocket;
+    
+    static int carBrokerPort;
+    static int hotelBrokerServer;
+    
+    static boolean carBrokerOnline, hotelBrokerOnline;
+    
+	public Server (int serverPort, InetAddress carBrokerAddress, int carBrokerPort, InetAddress hotelBrokerAddress, int hotelBrokerServer) {
+		this.carBrokerAddress = carBrokerAddress;
+		this.hotelBrokerAddress = hotelBrokerAddress;
+		carBrokerOnline = false;
+
+		try {
+			socket = new DatagramSocket(serverPort);
+		} catch (SocketException e) {
+			e.printStackTrace();
+		}
 		
+	}
+	
+	public static void main(String[] args) {
+		//Sending initial Messages to Broker
+
+		//initial CarBroker message
+		do {
+			checkCarBrokerAvailability();
+			if(!carBrokerOnline) {
+				try {
+					TimeUnit.SECONDS.sleep(5);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		} while (!carBrokerOnline);
+		
+		//initial HotelBroker message
+		do {
+			checkHotelBrokerAvailability();
+			if(!hotelBrokerOnline) {
+				try {
+					TimeUnit.SECONDS.sleep(5);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		} while (!hotelBrokerOnline);
+		
+		//Brokers are initialy available
+	
+		
+		
+		
+		
+	}
+	
+	public static void checkCarBrokerAvailability() {
+		try {
+			byte[] buf;
+			String msg = "0 InitialMessageRequest";
+			
+		    carBrokerSocket = new DatagramSocket();
+		    buf = msg.getBytes();
+		    DatagramPacket packet = new DatagramPacket(buf, buf.length, carBrokerAddress, carBrokerPort);
+		    carBrokerSocket.send(packet);
+		    //wait for answer
+		    packet = new DatagramPacket(buf, buf.length);
+		    carBrokerSocket.receive(packet);
+		    String response = new String(packet.getData(), 0, packet.getLength());
+		    if(response.equals("0 InitialMessageResponse")) {
+		    	carBrokerOnline = true;
+		    }
+	    } catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void checkHotelBrokerAvailability() {
+		try {
+			byte[] buf;
+			String msg = "0 InitialMessageRequest";
+			
+		    hotelBrokerSocket = new DatagramSocket();
+		    buf = msg.getBytes();
+		    DatagramPacket packet = new DatagramPacket(buf, buf.length, carBrokerAddress, carBrokerPort);
+		    hotelBrokerSocket.send(packet);
+		    //wait for answer
+		    packet = new DatagramPacket(buf, buf.length);
+		    hotelBrokerSocket.receive(packet);
+		    String response = new String(packet.getData(), 0, packet.getLength());
+		    if(response.equals("0 InitialMessageResponse")) {
+		    	hotelBrokerOnline = true;
+		    }
+	    } catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 }
--- src/main/java/Start.java
+++ src/main/java/Start.java
@@ -1,11 +1,25 @@
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 public class Start {
+	static InetAddress carBrokerAddress;
+	static InetAddress hotelBrokerAddress;
+	
 	public static void main(String[] args) {
 		int firstServerPort = 30800;
 		int secondServerPort = 30801;
+		try {
+			InetAddress carBrokerAddress = InetAddress.getByName("localhost");
+			InetAddress hotelBrokerAddress = InetAddress.getByName("localhost");
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		}
 		int carBrokerPort = 30901;
 		int hotelBrokerPort = 30902;
-		Server server1 = new Server(firstServerPort, carBrokerPort, hotelBrokerPort);
-		Server server2 = new Server(secondServerPort, carBrokerPort, hotelBrokerPort);
+		
+		//Start Broker
+		
+		Server server1 = new Server(firstServerPort, carBrokerAddress, carBrokerPort, hotelBrokerAddress, hotelBrokerPort);
+		Server server2 = new Server(secondServerPort, carBrokerAddress, carBrokerPort, hotelBrokerAddress, hotelBrokerPort);
 	}
 }
