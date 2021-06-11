8e3ec3639853d4efdce7ccfe3ab5c952d144b0fc
Christoph <christophfischerdhbw@gmail.com>
2021-06-11 11:46:26 +0200

    Added data and config files for Hotel and Car Service

--- src/main/java/CarService/CarBroker.java
+++ src/main/java/CarService/CarBroker.java
@@ -1,6 +1,8 @@
 package CarService;
 
 public class CarBroker {
+    private int serverPort;
+
     public static void main(String[] args) {
 
     }
--- src/main/java/HotelService/HotelBroker.java
+++ src/main/java/HotelService/HotelBroker.java
@@ -1,6 +1,7 @@
 package HotelService;
 
 public class HotelBroker {
+    private int serverPort;
 
     public static void main(String[] args) {
 
