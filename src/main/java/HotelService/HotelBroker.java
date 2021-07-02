a62cb80b9c71139caf3f9e2c871d2fb17be5c87f
Christoph <christophfischerdhbw@gmail.com>
2021-07-02 15:34:54 +0200

    Fixed merge

--- src/main/java/HotelService/HotelBroker.java
+++ src/main/java/HotelService/HotelBroker.java
@@ -7,7 +7,6 @@ import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.SocketException;
-import java.net.UnknownHostException;
 import java.util.Date;
 
 import Message.*;
