4f28f0e767e6b6c231fd8e1c265aa9d6ffc18223
Christoph <christophfischerdhbw@gmail.com>
2021-07-08 13:35:30 +0200

    Refactoring json handling for carpool class

--- src/main/java/CarService/CarPool.java
+++ src/main/java/CarService/CarPool.java
@@ -1,5 +1,6 @@
 package CarService;
 
+import JsonUtility.JsonHandler;
 import Message.StatusTypes;
 import Request.CarRequest;
 import org.json.simple.JSONArray;
@@ -15,12 +16,14 @@ import java.util.Date;
 
 public class CarPool {
     //Attribute
+    private final JsonHandler jsonHandler;
     private final ArrayList<Car> carList;
     private final ArrayList<CarRequest> requestList;
 
     public CarPool() {
         this.carList = new ArrayList<>();
         this.requestList = new ArrayList<>();
+        this.jsonHandler = new JsonHandler();
     }
 
     public boolean checkCarOfId(InetAddress target, int port, String bookingId, int carId, Date startTime, Date endTime) {
@@ -34,21 +37,17 @@ public class CarPool {
         JSONParser jParser = new JSONParser();
         try (FileReader reader = new FileReader("src/main/resources/CarService/data.json"))
         {
-            Object jsonContent = jParser.parse(reader);
-            JSONObject carData = (JSONObject) jsonContent;
-            Object roomDataContent = carData.get("cars");
-            JSONArray carJsonArray = (JSONArray) roomDataContent;
-            Object singleCarObject = carJsonArray.get(request.getCarId() - 1);
-            JSONObject singleCar = (JSONObject) singleCarObject;
-            Object reservationsObject = singleCar.get("Reservations");
-            JSONArray reservations = (JSONArray) reservationsObject;
+            JSONObject carsData = jsonHandler.getAttributeAsJsonObject(jParser.parse(reader));
+            JSONArray cars= jsonHandler.getAttributeAsJsonArray(carsData.get("cars"));
+            JSONObject singleCar = jsonHandler.getAttributeAsJsonObject(cars.get(request.getCarId() - 1));
+            JSONArray reservations = jsonHandler.getAttributeAsJsonArray(singleCar.get("Reservations"));
             JSONObject reservation = new JSONObject();
             reservation.put("Id", request.getId());
             reservation.put("StartTime", request.getStartTime().getTime());
             reservation.put("EndTime", request.getEndTime().getTime());
             reservations.add(reservation);
             try (FileWriter file = new FileWriter("src/main/resources/CarService/data.json")) {
-                file.write(carData.toJSONString());
+                file.write(carsData.toJSONString());
                 file.flush();
             } catch (IOException e) {
                 e.printStackTrace();
@@ -80,23 +79,18 @@ public class CarPool {
         JSONParser jParser = new JSONParser();
         try (FileReader reader = new FileReader("src/main/resources/CarService/data.json"))
         {
-            Object jsonContent = jParser.parse(reader);
-            JSONObject carData = (JSONObject) jsonContent;
-            Object carDataContent = carData.get("cars");
-            JSONArray carJsonArray = (JSONArray) carDataContent;
-            for (int i = 0; i < carJsonArray.size(); i++) {
-                Object singleCarData = carJsonArray.get(i);
-                JSONObject carInfo = (JSONObject) singleCarData;
+            JSONObject carsData = jsonHandler.getAttributeAsJsonObject(jParser.parse(reader));
+            JSONArray cars= jsonHandler.getAttributeAsJsonArray(carsData.get("cars"));
+            for (int i = 0; i < cars.size(); i++) {
+                JSONObject carInfo = jsonHandler.getAttributeAsJsonObject(cars.get(i));
                 Car singleCar = new Car(Integer.parseInt(carInfo.get("Id").toString()),carInfo.get("Manufacturer").toString(),
                         carInfo.get("Model").toString(),
                         Integer.parseInt(carInfo.get("HorsePower").toString()),
                         CarTypes.valueOf(carInfo.get("Type").toString())
                 );
-                Object reservationData = carInfo.get("Reservations");
-                JSONArray reservationJsonArray = (JSONArray) reservationData;
+                JSONArray reservationJsonArray = jsonHandler.getAttributeAsJsonArray(carInfo.get("Reservations"));
                 for(int j = 0; j < reservationJsonArray.size(); j++) {
-                    Object singleBooking = reservationJsonArray.get(j);
-                    JSONObject singleBookingData = (JSONObject) singleBooking;
+                    JSONObject singleBookingData = jsonHandler.getAttributeAsJsonObject(reservationJsonArray.get(j));
                     String startTime = singleBookingData.get("StartTime").toString();
                     String endTime = singleBookingData.get("EndTime").toString();
                     singleCar.bookCar(new Date(Long.parseLong(startTime)), new Date(Long.parseLong(endTime)));
@@ -108,13 +102,10 @@ public class CarPool {
         }
         try (FileReader reader = new FileReader("src/main/resources/CarService/requests.json"))
         {
-            Object jsonContent = jParser.parse(reader);
-            JSONObject requestsData = (JSONObject) jsonContent;
-            Object carRequestDataContent = requestsData.get("CarRequests");
-            JSONArray requests = (JSONArray) carRequestDataContent;
+            JSONObject requestsData = jsonHandler.getAttributeAsJsonObject(jParser.parse(reader));
+            JSONArray requests = jsonHandler.getAttributeAsJsonArray(requestsData.get("CarRequests"));
             for (int i = 0; i < requests.size(); i++) {
-                Object singleCarRequestData = requests.get(i);
-                JSONObject requestInfo = (JSONObject) singleCarRequestData;
+                JSONObject requestInfo = jsonHandler.getAttributeAsJsonObject(requests.get(i));
                 CarRequest singleCarRequest = new CarRequest(InetAddress.getByName(requestInfo.get("Target_IP").toString()),
                         Integer.parseInt(requestInfo.get("Target_Port").toString()),
                         requestInfo.get("BookingId").toString(),
@@ -147,10 +138,8 @@ public class CarPool {
         JSONParser jParser = new JSONParser();
         try (FileReader reader = new FileReader("src/main/resources/CarService/requests.json"))
         {
-            Object jsonContent = jParser.parse(reader);
-            JSONObject requestsData = (JSONObject) jsonContent;
-            Object carRequestDataContent = requestsData.get("CarRequests");
-            JSONArray carRequests = (JSONArray) carRequestDataContent;
+            JSONObject requestsData = jsonHandler.getAttributeAsJsonObject(jParser.parse(reader));
+            JSONArray carRequests = jsonHandler.getAttributeAsJsonArray(requestsData.get("CarRequests"));
             JSONObject carRequest = new JSONObject();
             carRequest.put("Target_IP", target.toString().replace("/", ""));
             carRequest.put("Target_Port", port);
@@ -187,13 +176,10 @@ public class CarPool {
         JSONParser jParser = new JSONParser();
         try (FileReader reader = new FileReader("src/main/resources/CarService/requests.json"))
         {
-            Object jsonContent = jParser.parse(reader);
-            JSONObject requestsData = (JSONObject) jsonContent;
-            Object carRequestDataContent = requestsData.get("CarRequests");
-            JSONArray carRequests = (JSONArray) carRequestDataContent;
+            JSONObject requestsData = jsonHandler.getAttributeAsJsonObject(jParser.parse(reader));
+            JSONArray carRequests = jsonHandler.getAttributeAsJsonArray(requestsData.get("CarRequests"));
             for(int i = 0; i < carRequests.size(); i++) {
-                Object requestData = carRequests.get(i);
-                JSONObject singleRequest = (JSONObject) requestData;
+                JSONObject singleRequest = jsonHandler.getAttributeAsJsonObject(carRequests.get(i));
                 if(singleRequest.get("BookingId").toString().equals(bookingId)) {
                     carRequests.remove(i);
                     break;
