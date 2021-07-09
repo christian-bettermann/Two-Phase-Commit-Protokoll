e210c1a1bbecf483d9761c9835d59543087b3eac
Christoph <christophfischerdhbw@gmail.com>
2021-07-09 09:19:46 +0200

    Fixed merge

--- src/main/java/ServerMessageHandler.java
+++ src/main/java/ServerMessageHandler.java
@@ -272,16 +272,18 @@ public class ServerMessageHandler implements Runnable{
 			JSONArray serverRequests = jsonHandler.getAttributeAsJsonArray(requestsData.get("ServerRequests"));
 			for(int i = 0; i < serverRequests.size(); i++) {
 				JSONObject singleRequest = jsonHandler.getAttributeAsJsonObject(serverRequests.get(i));
-				if(carState != null && !StatusTypes.valueOf(singleRequest.get("CarState").toString()).equals(carState)) {
-					singleRequest.replace("CarState", carState.toString());
-					updated = true;
-				}
-				if(hotelState != null && !StatusTypes.valueOf(singleRequest.get("HotelState").toString()).equals(hotelState)) {
-					singleRequest.replace("HotelState", hotelState.toString());
-					updated = true;
-				}
-				if(updated) {
-					break;
+				if(singleRequest.get("BookingId").toString().equals(bookingId)) {
+					if (carState != null && !StatusTypes.valueOf(singleRequest.get("CarState").toString()).equals(carState)) {
+						singleRequest.replace("CarState", carState.toString());
+						updated = true;
+					}
+					if (hotelState != null && !StatusTypes.valueOf(singleRequest.get("HotelState").toString()).equals(hotelState)) {
+						singleRequest.replace("HotelState", hotelState.toString());
+						updated = true;
+					}
+					if (updated) {
+						break;
+					}
 				}
 			}
 			try (FileWriter file = new FileWriter(this.requestFilePath)) {
